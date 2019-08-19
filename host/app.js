8ad66b72bd0feda46da5f9e984075a0c552e5399
lmaran <lucian.maran@outlook.com>
2019-08-19 19:59:53 +0000

    wip [skip ci]

--- host/app.js
+++ host/app.js
@@ -1,24 +1,36 @@
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
-
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 const addUserIfExist = require("./middlewares/addUserIfExist.middleware").addUserIfExist;
 
 const user = require("../user/app");
 const api = require("../api/app");
 const web = require("../web/app");
+const mongoHelper = require("../shared/helpers/mongo.helper");
 
 const app = express();
+
+const mongoClientAsPromise = mongoHelper.init();
 
 // routes for static files; in prod set NGINX to serve them
 // app.use("/", express.static(path.join(__dirname, "../public"), { maxAge: 31557600000 })); // one year in milliseconds
 app.use("/", express.static(path.join(__dirname, "../public")));
 app.use("/lib/lit-html", express.static(path.join(__dirname, "../../node_modules/lit-html")));
 
-app.get("/check", function (req, res) {
+app.get("/check", function(req, res) {
     res.send("matemaraton-" + (process.env.DEPLOYMENT_SLOT || "noslot") + "-" + process.env.NODE_ENV);
 });
 
+app.use(
+    session({
+        secret: "foo",
+        resave: false,
+        saveUninitialized: true,
+        store: new MongoStore({ clientPromise: mongoClientAsPromise })
+    })
+);
 app.use(cookieParser()); // Parse Cookie header and populate req.cookies with an object
 app.use(addUserIfExist); // verify jwt token and populate req.user (depends on "cookie-parser")
 
@@ -27,12 +39,12 @@ app.use("/", user);
 app.use("/", web);
 
 app.use((req, res, next) => {
-    res.status(404).send("Sorry can't find that!")
-})
+    res.status(404).send("Sorry can't find that!");
+});
 
 app.use((err, req, res, next) => {
-    console.error(err.stack)
-    res.status(500).send('Something broke!')
-})
+    console.error(err.stack);
+    res.status(500).send("Something broke!");
+});
 
 module.exports = app;
--- shared/helpers/mongo.helper.js
+++ shared/helpers/mongo.helper.js
@@ -5,7 +5,72 @@ const MongoClient = mongodb.MongoClient;
 const ObjectID = mongodb.ObjectID;
 let theDb; // this will be re-used so the db is only created once (on first request).
 
+let _db;
+let _clientAsPromise;
+
+// (async function() {
+//   // Connection URL
+//   const url = config.mongo.uri;
+//   // Database Name
+//   const dbName = onfig.mongo.dbName;
+//   const client = new MongoClient(url, { useNewUrlParser: true });
+
+//   try {
+//     // Use connect method to connect to the Server
+//     await client.connect();
+
+//     const db = client.db(dbName);
+//   } catch (err) {
+//     console.log(err.stack);
+//   }
+
+//   client.close();
+// })();
+
 exports.getDb = async () => {
+    try {
+        if (!_db) {
+            const client = await _clientAsPromise;
+            const db = client.db(config.mongo.dbName);
+            _db = db;
+            return db;
+        } else {
+            // db already exists...
+            return _db;
+        }
+    } catch (error) {
+        throw new Error(error);
+    }
+};
+
+exports.getClientAsPromise = () => {
+    return _clientAsPromise;
+};
+
+// https://mongodb.github.io/node-mongodb-native/3.3/reference/ecmascriptnext/connecting/
+exports.init = () => {
+    try {
+        if (!config.mongo || !config.mongo.uri) {
+            throw new Error("Nu este definit un connection string pentru Mongo.");
+        }
+        if (!config.mongo || !config.mongo.dbName) {
+            throw new Error("Nu este definit numele bazei de date.");
+        }
+        // const client = new MongoClient(config.mongo.uri, { useNewUrlParser: true });
+
+        // await client.connect();
+
+        // sconst db = client.db(dbName);
+
+        const clientAsPromise = MongoClient.connect(config.mongo.uri + "/matemaraton-dev", config.mongo.options);
+        _clientAsPromise = clientAsPromise;
+        return clientAsPromise;
+    } catch (error) {
+        throw new Error(error);
+    }
+};
+
+exports.getDb_old = async () => {
     try {
         if (!theDb) {
             if (!config.mongo || !config.mongo.uri) {
