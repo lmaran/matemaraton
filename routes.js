4e6d5d2abf27df86933bea23bb6b80a72f1e852e
lmaran <lucian.maran@outlook.com>
2019-11-06 18:38:48 +0000

    bug fix: pregatire simulare en  ed2 [skip ci]

--- routes.js
+++ routes.js
@@ -42,7 +42,7 @@ router.get("/clase/:classId/elevi", studentController.getStudentsPerClass);
 // router.get("/matemaraton/:edition", matemaratonController.getEditionHomepage);
 // router.get("/:edition?/prezenta/grupe/:groupId", matemaratonController.getPresencePerGroup);
 // router.get("/:edition?/prezenta/elevi/:studentId", matemaratonController.getPresencePerStudent);
-// router.get("/:edition?/pregatire-simulare-en", matemaratonController.getTrainingProgramForENSimulation);
+router.get("/pregatire-simulare-en-editia-2", matemaratonController.getTrainingProgramForENSimulationEdition2);
 // router.get("/:edition?/cursuri/grupe/:groupId", matemaratonController.getCoursesPerGroup);
 // router.get("/:edition?/cursuri/:courseId", matemaratonController.getCourse);
 router.get("/:edition?", matemaratonController.getMatemaraton);
