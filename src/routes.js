daafb33176d47290762e2200da3efb35db3d9a45
Lucian Maran <lucian.maran@outlook.com>
2020-02-08 20:30:45 +0200

    fix some routes [skip ci]

--- src/routes.js
+++ src/routes.js
@@ -10,8 +10,6 @@ const studentController = require("./controllers/student.controller");
 const parentController = require("./controllers/parent.controller");
 
 const contactController = require("./controllers/contact.controller");
-const pageController = require("./controllers/page.controller");
-// const userController = require("./controllers/user.controller");
 const userResetPasswordController = require("./controllers/user-reset-password.controller");
 const userChangePasswordController = require("./controllers/user-change-password.controller");
 const userSignupController = require("./controllers/user-signup.controller");
@@ -59,9 +57,6 @@ router.get("/cursuri/:courseId", courseController.getCourse);
 router.get("/clase/:classId/elevi", studentController.getStudentsPerClass);
 router.get("/elevi/:studentId", studentController.getStudent);
 
-// router.get("/matemaraton/:edition", matemaratonController.getEditionHomepage);
-// router.get("/:edition?/prezenta/grupe/:groupId", matemaratonController.getPresencePerGroup);
-// router.get("/:edition?/prezenta/elevi/:studentId", matemaratonController.getPresencePerStudent);
 router.get("/pregatire-simulare-en-editia-2", matemaratonController.getTrainingProgramForENSimulationEdition2);
 router.get("/pregatire-teza-s1-editia-3", matemaratonController.getTrainingProgramForSemestrialPaperEdition3);
 router.get("/materiale-teza-s1", matemaratonController.getDocumentsForSemestrialPaper);
@@ -72,8 +67,6 @@ router.get("/contact", contactController.getContact);
 // problems
 router.get("/probleme/:id", problemController.getProblem);
 router.post("/probleme", problemController.createProblem);
-
-router.get("/:pageId/asdfgh", pageController.getPage2);
 
 // user-login/logout
 router.get("/login", userLoginController.getLogin);
@@ -99,11 +92,6 @@ router.post("/reset-password", userResetPasswordController.postResetPassword);
 router.get("/reset-password/ask-to-confirm", userResetPasswordController.displayResetPasswordAskToConfirm);
 router.get("/reset-password/confirm/:resetPasswordCode", userResetPasswordController.getResetPasswordConfirm);
 
-// router.get("/:edition?/cursuri/grupe/:groupId", matemaratonController.getCoursesPerGroup);
-// router.get("/:edition?/cursuri/:courseId", matemaratonController.getCourse);
-// router.get("/:edition?", matemaratonController.getMatemaraton);
-
-// pages
-router.get("/:pageId", pageController.getPage);
+router.get("/:edition?", matemaratonController.getMatemaraton);
 
 module.exports = router;
