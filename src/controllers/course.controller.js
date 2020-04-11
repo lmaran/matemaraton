6f37dcb64361af18ad4599a61631b9e829491d62
Lucian Maran <lucian.maran@outlook.com>
2020-04-11 20:03:44 +0300

    add youtube videos; add high quality pictures;

--- src/controllers/course.controller.js
+++ src/controllers/course.controller.js
@@ -26,12 +26,39 @@ exports.getCoursesPerClass = async (req, res) => {
     res.render("course/courses-per-class", data);
 };
 
+exports.getCoursesPerClassWithPhotos = async (req, res) => {
+    const classId = req.params.classId;
+
+    const [cls, courses] = await Promise.all([
+        await classService.getClassById(classId),
+        await courseService.getCoursesByClassId(classId)
+    ]);
+
+    const newCourses = courses
+        .filter(x => !x.noCourse)
+        .map(x => {
+            x.dateAsString = dateTimeHelper.getStringFromStringNoDay(x.date);
+            x.images.forEach(image => (image.highQualityUrl = image.url.replace("courses-lg", "courses")));
+            return x;
+        });
+
+    const data = {
+        class: cls,
+        courses: newCourses,
+        totalCourses: newCourses.length
+    };
+    //res.send(data);
+    res.render("course/courses-with-all-photos", data);
+};
+
 exports.getCourse = async (req, res) => {
     const courseId = req.params.courseId;
 
     const course = await courseService.getCourseById(courseId);
 
     course.dateAsString = dateTimeHelper.getStringFromStringNoDay(course.date);
+
+    course.images.forEach(image => (image.highQualityUrl = image.url.replace("courses-lg", "courses")));
 
     const data = {
         course
