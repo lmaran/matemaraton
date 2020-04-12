0dbf9a1ef210b73749888c933aad03e9dfc3ddad
Lucian Maran <lucian.maran@outlook.com>
2020-04-12 18:49:45 +0300

    add a notification info about the new videos

--- src/helpers/handlebar.helper.js
+++ src/helpers/handlebar.helper.js
@@ -1,40 +1,43 @@
 module.exports = {
-    section: function (name, options) {
+    section: function(name, options) {
         if (!this._sections) {
             this._sections = {};
         }
         this._sections[name] = options.fn(this);
         return null;
     },
-    toJSON: function (object) {
+    toJSON: function(object) {
         return JSON.stringify(object);
     },
-    eq: function (v1, v2) {
+    eq: function(v1, v2) {
         return v1 === v2;
     },
-    ne: function (v1, v2) {
+    eq2: function(v1, v2) {
+        return v1 == v2;
+    },
+    ne: function(v1, v2) {
         return v1 !== v2;
     },
-    lt: function (v1, v2) {
+    lt: function(v1, v2) {
         return v1 < v2;
     },
-    gt: function (v1, v2) {
+    gt: function(v1, v2) {
         return v1 > v2;
     },
-    lte: function (v1, v2) {
+    lte: function(v1, v2) {
         return v1 <= v2;
     },
-    gte: function (v1, v2) {
+    gte: function(v1, v2) {
         return v1 >= v2;
     },
-    and: function () {
+    and: function() {
         return Array.prototype.slice.call(arguments, 0, arguments.length - 1).every(Boolean);
     },
-    or: function () {
+    or: function() {
         return Array.prototype.slice.call(arguments, 0, arguments.length - 1).some(Boolean);
     },
-    inc: function (v) {
+    inc: function(v) {
         // https://stackoverflow.com/a/22103990
         return parseInt(v) + 1;
     }
-}
+};
