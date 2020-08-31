5e974d554f15dd5e5e2d33f68dca0fac54653a99
Lucian Maran <lucian.maran@visma.com>
2020-08-31 19:43:40 +0300

    upload files, wip [skip ci]

--- src/controllers/file.controller.js
+++ src/controllers/file.controller.js
@@ -6,6 +6,7 @@ const fileSizeLimit = 100 * 1024 * 1024; // 100 MB
 
 const config = require("../config");
 //const streamHelper = require("../helpers/stream.helper");
+const stringHelper = require("../helpers/string.helper");
 const { BlobServiceClient } = require("@azure/storage-blob");
 const ONE_MEGABYTE = 1024 * 1024;
 const uploadOptions = { bufferSize: 4 * ONE_MEGABYTE, maxBuffers: 20 };
@@ -14,22 +15,26 @@ const uploadOptions = { bufferSize: 4 * ONE_MEGABYTE, maxBuffers: 20 };
 // const blobServiceClient = BlobServiceClient.fromConnectionString(config.azureBlobStorageConnectionString);
 // const containerClient = blobServiceClient.getContainerClient("counters");
 
-exports.upload = async (req, res) => {
+exports.upload_Old = async (req, res) => {
     // https://stackoverflow.com/a/59295385
+    // https://stackoverflow.com/a/29996871/2726725
+
     // res.send("bbb");
 
     const startTime = Date.now();
     let batchItems = [];
     const batchSize = 1000; // this is also default batchSize in Mongodb la BulkWrite (internally used by InsertMany)
 
+    //const inputFileNames = [];
     const result = [];
+    const result2 = [];
     let files = 0,
         finished = false;
 
     // I abort upload if file is over 10 MB limit
     const busboy = new Busboy({ headers: req.headers, limits: { fileSize: fileSizeLimit } });
     busboy.on("file", async function(fieldname, uploadFileStream, fileName, encoding, mimeType) {
-        result.push(fileName);
+        result.push({ fileName });
         ++files;
         //validate against empty file name
         if (fileName.length > 0) {
@@ -118,7 +123,10 @@ exports.upload = async (req, res) => {
             const blobServiceClient = BlobServiceClient.fromConnectionString(config.azureBlobStorageConnectionString);
             const containerClient = blobServiceClient.getContainerClient("homework-submissions");
 
-            const blockBlobClient = containerClient.getBlockBlobClient(fileName);
+            // "Cuş Cuş.jpg" --> "5f4bfb45d8278706d442058c.jpg"
+            const blobName = stringHelper.getUniqueFileName(fileName);
+
+            const blockBlobClient = containerClient.getBlockBlobClient(blobName);
 
             try {
                 await blockBlobClient.uploadStream(
@@ -128,7 +136,16 @@ exports.upload = async (req, res) => {
                     { blobHTTPHeaders: { blobContentType: mimeType } }
                 );
                 //res.render("success", { message: "File uploaded to Azure Blob storage." });
-                console.log(fileName + ": done!");
+                //console.log(fileName + ": done!");
+                const fileInResult = result.find(x => x.fileName === fileName);
+
+                fileInResult.url =
+                    "https://" +
+                    blobServiceClient.accountName +
+                    ".blob.core.windows.net/files/5f4bfb45d8278706d442058c-lg.jpg";
+                console.log(fileInResult);
+                result2.push(fileInResult);
+                console.log(fileName + " aaa");
 
                 // if (--files === 0 && finished) {
                 //     res.writeHead(200, { Connection: "close" });
@@ -145,10 +162,99 @@ exports.upload = async (req, res) => {
 
     busboy.on("finish", function() {
         console.log("Upload completed!");
+        console.log(result2);
         res.writeHead(200, { Connection: "close" });
         //res.end("That's all folks!");
-        res.end(JSON.stringify(result));
+        res.end(JSON.stringify(result2));
         //finished = true;
+    });
+
+    return req.pipe(busboy);
+};
+
+exports.upload = async (req, res) => {
+    // https://stackoverflow.com/a/59295385
+    // https://stackoverflow.com/a/29996871/2726725
+
+    const result = [];
+    //const result2 = [];
+    let files = 0,
+        finished = false;
+
+    // I abort upload if file is over 10 MB limit
+    const busboy = new Busboy({ headers: req.headers, limits: { fileSize: fileSizeLimit } });
+    busboy.on("file", async function(fieldname, uploadFileStream, fileName, encoding, mimeType) {
+        result.push({ fileName });
+        ++files;
+        //validate against empty file name
+        if (fileName.length > 0) {
+            console.log(
+                "File [" +
+                    fieldname +
+                    "]: filename: " +
+                    fileName +
+                    ", encoding: " +
+                    encoding +
+                    ", mimetype: " +
+                    mimeType
+            );
+            // uploadFileStream.on("data", function(data) {
+            //     console.log("File [" + fieldname + "] got " + data.length + " bytes");
+            // });
+            // uploadFileStream.on("end", function() {
+            //     console.log("File [" + fieldname + "] Finished");
+            // });
+
+            // const filePath = path.join(__dirname, "uploads/" + filename);
+            uploadFileStream.on("limit", function() {
+                console.log(`file size over ${fileSizeLimit / (1024 * 1024)} MB.`);
+            });
+
+            const blobServiceClient = BlobServiceClient.fromConnectionString(config.azureBlobStorageConnectionString);
+            const containerClient = blobServiceClient.getContainerClient("files");
+
+            // "Cuş Cuş.jpg" --> "5f4bfb45d8278706d442058c.jpg"
+            const blobName = stringHelper.getUniqueFileName(fileName);
+
+            const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+
+            try {
+                await blockBlobClient.uploadStream(
+                    uploadFileStream,
+                    uploadOptions.bufferSize,
+                    uploadOptions.maxBuffers,
+                    { blobHTTPHeaders: { blobContentType: mimeType } }
+                );
+                //res.render("success", { message: "File uploaded to Azure Blob storage." });
+                //console.log(fileName + ": done!");
+                const fileInResult = result.find(x => x.fileName === fileName);
+
+                fileInResult.url =
+                    "https://" + blobServiceClient.accountName + ".blob.core.windows.net/files/" + blobName;
+                //console.log(fileInResult);
+                //result.push(fileInResult);
+                //console.log(fileName + " aaa");
+
+                if (--files === 0 && finished) {
+                    //res.writeHead(200, { Connection: "close" });
+                    console.log("Upload completed and saved to Blob!");
+                    console.log(result);
+                    //res.end(JSON.stringify(result));
+                    res.json(result);
+                    //res.end("");
+                }
+            } catch (err) {
+                //res.render("error", { message: err.message });
+            }
+        } else {
+            console.log("empty file name");
+            uploadFileStream.resume();
+        }
+    });
+
+    busboy.on("finish", function() {
+        console.log("Upload completed!");
+        finished = true;
     });
 
     return req.pipe(busboy);
--- src/helpers/string.helper.js
+++ src/helpers/string.helper.js
@@ -1,3 +1,6 @@
+const path = require("path");
+const { ObjectID } = require("mongodb");
+
 exports.endsWithValueFromList = (str, endValues) => {
     return endValues.some(v => str.endsWith(v));
 };
@@ -8,4 +11,18 @@ exports.endsWithValueFromList = (str, endValues) => {
  */
 exports.getIntervalFromAcademicYear = academicYear => {
     return `${academicYear.substr(0, 4)} - 20${academicYear.substr(4, 2)}`;
+};
+
+/**
+ * input: "Cuş Cuş.jpg"
+ * output: "5f4bfb45d8278706d442058c.jpg"
+ * @param {string} fileName
+ */
+exports.getUniqueFileName = fileName => {
+    const parsedFile = path.parse(fileName); // Cuş Cuş.jpg
+    // const fileNameWithoutExtension = parsedFile.name; // Cuş Cuş
+    const fileExtensionWithDot = parsedFile.ext; // .jpg
+
+    //const uniqueIdentifier = new ObjectID();
+    return new ObjectID() + fileExtensionWithDot;
 };
