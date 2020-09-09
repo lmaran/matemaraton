5e573e385b3831fb839ddb923955e0bbaf98bc68
Lucian Maran <lucian.maran@outlook.com>
2020-09-09 19:41:09 +0300

    add static validation for enrollment form [skip ci]

--- src/controllers/enrollment.controller.js
+++ src/controllers/enrollment.controller.js
@@ -1,64 +1,67 @@
+const validator = require("validator");
 const classService = require("../services/class.service");
 const enrollService = require("../services/enroll.service");
 const dateTimeHelper = require("../helpers/date-time.helper");
+//const arrayHelper = require("../helpers/array.helper");
 
 exports.enrollInClassGet = async (req, res) => {
     const classId = req.params.classId;
-    const classes = await classService.getOpenEnrollments();
-    const cls = await classService.getOneById(classId);
 
-    const data = {
-        user: req.user,
-        class: cls,
-        selectedClassId: classId,
-        classes,
-        ctx: req.ctx,
+    // Get an array of flash errors (or initial values) by passing the key
+    const validationErrors = req.flash("validationErrors");
+    const initialValues = req.flash("initialValues");
 
-        can: {
-            //viewParentsLink: await autz.can(req.user, "read:parents", { classId })
-        }
-    };
+    // const errors = arrayHelper.arrayToObject(validationErrors, "field");
+    let errors = {};
+    if (validationErrors.length > 0) {
+        errors = validationErrors[0];
+    }
+
+    let data = {};
+
+    if (initialValues.length === 0) {
+        // initial request
+        const cls = await classService.getOneById(classId);
+
+        data.classId = cls._id;
+        data.className = cls.name;
+        data.classLevel = cls.level;
+    } else {
+        //redirect after validation
+        data = initialValues[0];
+    }
 
     //res.send(data);
-    res.render("enroll/enroll-in-class", { data });
+    res.render("enroll/enroll-in-class", { data, errors });
 };
 
 exports.enrollInClassPost = async (req, res) => {
     try {
-        const {
-            parentId,
-            studentLastName,
-            studentFirstName,
-            classId,
-            mathAvgGrade1,
-            mathAvgGrade2,
-            schoolName,
-            observations
-        } = req.body;
+        // handle static validation errors
+        const validationErrors = getStaticValidationErrors(req);
+
+        if (Object.keys(validationErrors).length > 0) {
+            //if not empty object
+            return flashAndReloadPage(req, res, validationErrors);
+        }
 
         const enroll = {
-            parentId,
-            studentLastName,
-            studentFirstName,
-            classId,
-            mathAvgGrade1,
-            mathAvgGrade2,
-            schoolName,
-            observations,
+            classId: req.body.classId,
+            parentId: req.user._id,
+            studentLastName: req.body.studentLastName,
+            studentFirstName: req.body.studentFirstName,
+            mathAvgGrade1: req.body.mathAvgGrade1,
+            mathAvgGrade2: req.body.mathAvgGrade2,
+            schoolName: req.body.schoolName,
+            observations: req.body.observations,
             createdOn: new Date(),
-            createdBy: parentId
+            createdBy: req.user._id
         };
 
         await enrollService.insertOne(enroll);
 
-        // // handle static validation errors
-        // const validationErrors = getSignupStaticValidationErrors(firstName, lastName, email, password, confirmPassword);
-        // if (validationErrors.length) {
-        //     return flashAndReloadSignupPage(req, res, validationErrors);
-        // }
-
         // res.redirect("/enroll/confirm-success");
-        res.redirect(`/clase/${classId}/inscrieri`);
+        res.redirect(`/clase/${req.body.classId}/inscrieri`);
     } catch (err) {
         // // handle dynamic validation errors
         // const validationErrors = [];
@@ -125,3 +128,47 @@ function convertDateToUTC(date) {
         date.getUTCSeconds()
     );
 }
+
+const getStaticValidationErrors = req => {
+    const { studentLastName, studentFirstName, mathAvgGrade1, mathAvgGrade2, schoolName, observations } = req.body;
+    try {
+        const validationErrors = {};
+
+        // studentLastName
+        if (validator.isEmpty(studentLastName)) validationErrors.studentLastName = { msg: "Câmp obligatoriu" };
+        else if (!validator.isLength(studentLastName, { max: 50 }))
+            validationErrors.studentLastName = { msg: "Maxim 50 caractere" };
+
+        // studentFirstName
+        if (validator.isEmpty(studentFirstName)) validationErrors.studentFirstName = { msg: "Câmp obligatoriu" };
+        else if (!validator.isLength(studentFirstName, { max: 50 }))
+            validationErrors.studentFirstName = { msg: "Maxim 50 caractere" };
+
+        // mathAvgGrade1
+        if (!validator.isLength(mathAvgGrade1, { max: 50 }))
+            validationErrors.mathAvgGrade1 = { msg: "Maxim 50 caractere" };
+
+        // mathAvgGrade2
+        if (!validator.isLength(mathAvgGrade2, { max: 50 }))
+            validationErrors.mathAvgGrade2 = { msg: "Maxim 50 caractere" };
+
+        // schoolName
+        if (!validator.isLength(schoolName, { max: 50 })) validationErrors.schoolName = { msg: "Maxim 50 caractere" };
+
+        // observations
+        if (!validator.isLength(observations, { max: 250 }))
+            validationErrors.observations = { msg: "Maxim 250 caractere" };
+
+        return validationErrors;
+    } catch (err) {
+        throw new Error(err);
+    }
+};
+
+const flashAndReloadPage = (req, res, validationErrors) => {
+    // keep old values at page reload by setting a flash message (a key, followed by a value)
+    req.flash("validationErrors", validationErrors);
+    req.flash("initialValues", req.body); // the initialValues are automatically sent as an array
+    const currentUrl = req.get("referer"); // "/signup?invitationCode=..."
+    return res.redirect(currentUrl);
+};
