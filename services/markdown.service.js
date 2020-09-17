b0a3a3ec7ccff58f36e889aea2bbca2641dc3f9b
lmaran <lucian.maran@visma.com>
2020-09-17 06:51:00 +0000

    fix: in Katex/Chrome dividing lines for \frac sometimes disappear [skip-ci] [skip ci]

--- services/markdown.service.js
+++ services/markdown.service.js
@@ -1,7 +1,16 @@
 const katex = require("katex");
-const tm = require("markdown-it-texmath").use(katex);
-const md = require("markdown-it")().use(tm, { delimiters: "dollars", macros: { "\\RR": "\\mathbb{R}" } });
+const tm = require("markdown-it-texmath");
+const md = require("markdown-it")().use(tm, {
+    engine: katex,
+    delimiters: "dollars",
+    katexOptions: { macros: { "\\RR": "\\mathbb{R}" }, minRuleThickness: 0.05 },
+});
+// added "minRuleThickness" option because dividing lines for \frac and \dfrac sometimes disappear (Chrome, zoom 100%)
+// default value is 0.04 (as defined in Latex specs) and seems that Chrome rounds down  sub-pixel lines
+// https://github.com/KaTeX/KaTeX/issues/1775#issuecomment-449762459
+// https://github.com/KaTeX/KaTeX/pull/1964
+// https://github.com/KaTeX/KaTeX/issues/824
 
-exports.render = katex => {
+exports.render = (katex) => {
     return md.render(katex);
 };
