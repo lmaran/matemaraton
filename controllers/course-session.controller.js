755a504997469f6db59f2b6bcd72c235f2a9ee52
lmaran <lucian.maran@outlook.com>
2020-10-11 18:03:57 +0000

    add CRUD for course session [skip ci]

--- controllers/course-session.controller.js
+++ controllers/course-session.controller.js
@@ -1,6 +1,129 @@
 const courseSessionService = require("../services/course-session.service");
 const classService = require("../services/class.service");
 const dateTimeHelper = require("../helpers/date-time.helper");
+const autz = require("../services/autz.service");
+const studentsAndClassesService = require("../services/studentsAndClasses.service");
+const personService = require("../services/person.service");
+
+exports.deleteOneById = async (req, res) => {
+    const canDeleteCourseSession = await autz.can(req.user, "delete:course-session");
+    if (!canDeleteCourseSession) {
+        return res.status(403).send("Lipsă permisiuni!"); // forbidden
+    }
+    courseSessionService.deleteOneById(req.body.id);
+    res.redirect(`/clase/${req.body.classId}/sesiuni-curs`);
+};
+
+exports.createOrEditGet = async (req, res) => {
+    const canCreateOrEditCourseSession = await autz.can(req.user, "create-or-edit:course-session");
+    if (!canCreateOrEditCourseSession) {
+        return res.status(403).send("Lipsă permisiuni!"); // forbidden
+    }
+
+    const isEditMode = !!req.params.id;
+
+    const data = {
+        isEditMode,
+    };
+
+    let classId;
+    if (isEditMode) {
+        const courseSession = await courseSessionService.getOneById(req.params.id);
+        classId = courseSession.classId;
+        data.courseSession = courseSession;
+
+        if (courseSession.images) {
+            courseSession.images = courseSession.images.reduce((acc, crt) => {
+                acc += crt.url + "\n";
+                return acc;
+            }, "");
+        }
+
+        if (courseSession.videos) {
+            courseSession.videos = courseSession.videos.reduce((acc, crt) => {
+                acc += crt.url + "\n";
+                return acc;
+            }, "");
+        }
+    } else {
+        classId = req.params.classId;
+        const numberOfSessionForClass = await courseSessionService.getNumberOfSessionForClass(classId);
+        data.courseSession = {
+            course: numberOfSessionForClass + 1,
+            date: dateTimeHelper.getStringFromDate(new Date()),
+        };
+    }
+
+    const [cls, studentsMapByClassId] = await Promise.all([
+        await classService.getOneById(classId),
+        await studentsAndClassesService.getStudentsMapByClassId(classId),
+    ]);
+
+    const allStudentsIdsPerClass = studentsMapByClassId.map((x) => x.studentId);
+
+    data.class = cls;
+
+    const allStudentsPerClass = await personService.getAllByIds(allStudentsIdsPerClass);
+
+    if (data.courseSession.studentsIds) {
+        allStudentsPerClass.forEach((student) => {
+            student.isPresent = data.courseSession.studentsIds.includes(student._id.toString());
+        });
+    }
+    data.students = allStudentsPerClass;
+
+    //res.send(data);
+    res.render("course-session/course-session-create-or-edit", data);
+};
+
+exports.createOrEditPost = async (req, res) => {
+    try {
+        const canCreateOrEditCourseSession = await autz.can(req.user, "create-or-edit:course-session");
+        if (!canCreateOrEditCourseSession) {
+            return res.status(403).send("Lipsă permisiuni!"); // forbidden
+        }
+        //const classId = req.params.classId;
+        const { id, classId, course, date, description, studentsIds, images, videos } = req.body;
+
+        const isEditMode = !!id;
+
+        const courseSession = {
+            course,
+            classId,
+            date,
+            description,
+            studentsIds,
+        };
+
+        if (images) {
+            const imagesUrls = images.split("\n").filter((x) => x.trim() != "");
+            courseSession.images = imagesUrls.map((imagesUrl) => {
+                return { url: imagesUrl.trim() };
+            });
+        }
+
+        if (videos) {
+            const videosUrls = videos.split("\n").filter((x) => x.trim() != "");
+            courseSession.videos = videosUrls.map((videosUrl) => {
+                return { url: videosUrl.trim() };
+            });
+        }
+
+        if (isEditMode) {
+            courseSession._id = id;
+            courseSession.classId;
+            courseSessionService.updateOne(courseSession);
+        } else {
+            const result = await courseSessionService.insertOne(courseSession);
+            courseSession._id = result.insertedId;
+        }
+
+        //res.send(courseSession);
+        res.redirect(`/clase/${classId}/sesiuni-curs`);
+    } catch (err) {
+        return res.status(500).json(err.message);
+    }
+};
 
 exports.getCourseSessionsPerClass = async (req, res) => {
     const classId = req.params.classId;
@@ -21,6 +144,7 @@ exports.getCourseSessionsPerClass = async (req, res) => {
         class: cls,
         courses: newCourses,
         totalCourses: newCourses.length,
+        canCreateOrEditCourseSession: await autz.can(req.user, "create-or-edit:course-session"),
     };
     //res.send(data);
     res.render("course-session/course-sessions-per-class", data);
@@ -54,18 +178,19 @@ exports.getCourseSessionsPerClassWithPhotos = async (req, res) => {
 exports.getCourseSession = async (req, res) => {
     const courseSessionId = req.params.courseSessionId;
 
-    const course = await courseSessionService.getOneById(courseSessionId);
-    const cls = await classService.getOneById(course.classId);
+    const courseSession = await courseSessionService.getOneById(courseSessionId);
+    const cls = await classService.getOneById(courseSession.classId);
 
-    course.dateAsString = dateTimeHelper.getStringFromStringNoDay(course.date);
+    courseSession.dateAsString = dateTimeHelper.getStringFromStringNoDay(courseSession.date);
 
-    if (course.images) {
-        course.images.forEach((image) => (image.highQualityUrl = image.url.replace("courses-lg", "courses")));
+    if (courseSession.images) {
+        courseSession.images.forEach((image) => (image.highQualityUrl = image.url.replace("courses-lg", "courses")));
     }
 
     const data = {
         class: cls,
-        course,
+        courseSession,
+        canCreateOrEditCourseSession: await autz.can(req.user, "create-or-edit:course-session"),
     };
     //res.send(data);
     res.render("course-session/course-session", data);
--- services/course-session.service.js
+++ services/course-session.service.js
@@ -3,20 +3,20 @@ const { ObjectID } = require("mongodb");
 
 const collection = "courseSessions";
 
-exports.getOneById = async id => {
+exports.getOneById = async (id) => {
     const db = await mongoHelper.getDb();
     return db.collection(collection).findOne({ _id: new ObjectID(id) });
 };
 
-exports.getAllByIds = async ids => {
+exports.getAllByIds = async (ids) => {
     const db = await mongoHelper.getDb();
     return db
         .collection(collection)
-        .find({ _id: { $in: ids.map(x => new ObjectID(x)) } })
+        .find({ _id: { $in: ids.map((x) => new ObjectID(x)) } })
         .toArray();
 };
 
-exports.getCourseSessionsByStudentId = async studentId => {
+exports.getCourseSessionsByStudentId = async (studentId) => {
     const db = await mongoHelper.getDb();
     return db
         .collection(collection)
@@ -24,26 +24,25 @@ exports.getCourseSessionsByStudentId = async studentId => {
         .toArray();
 };
 
-exports.getCourseSessionsByStudentsIds = async studentsIds => {
+exports.getCourseSessionsByStudentsIds = async (studentsIds) => {
     const db = await mongoHelper.getDb();
     return db
         .collection(collection)
         .find({
-            $or: [{ studentsIds: { $in: studentsIds } }, { "studentsFromOtherClasses.studentId": { $in: studentsIds } }]
+            $or: [
+                { studentsIds: { $in: studentsIds } },
+                { "studentsFromOtherClasses.studentId": { $in: studentsIds } },
+            ],
         })
         .toArray();
 };
 
-exports.getCourseSessionsByClassId = async classId => {
+exports.getCourseSessionsByClassId = async (classId) => {
     const db = await mongoHelper.getDb();
-    return db
-        .collection(collection)
-        .find({ classId })
-        .sort({ date: -1 })
-        .toArray();
+    return db.collection(collection).find({ classId }).sort({ date: -1 }).toArray();
 };
 
-exports.getCourseSessionsByClassIds = async classIds => {
+exports.getCourseSessionsByClassIds = async (classIds) => {
     const db = await mongoHelper.getDb();
     return db
         .collection(collection)
@@ -51,7 +50,30 @@ exports.getCourseSessionsByClassIds = async classIds => {
         .toArray();
 };
 
-exports.bulkWriteCourses = async mongoOps => {
+exports.getNumberOfSessionForClass = async (classId) => {
+    console.log(classId);
+    const db = await mongoHelper.getDb();
+    return db.collection(collection).countDocuments({ classId });
+};
+
+exports.bulkWriteCourses = async (mongoOps) => {
     const db = await mongoHelper.getDb();
     return db.collection(collection).bulkWrite(mongoOps, { ordered: false });
+};
+
+exports.insertOne = async (item) => {
+    const db = await mongoHelper.getDb();
+    //item.createdOn = new Date();
+    return db.collection(collection).insertOne(item);
+};
+
+exports.updateOne = async (item) => {
+    const db = await mongoHelper.getDb();
+    item._id = new ObjectID(item._id);
+    return db.collection(collection).updateOne({ _id: item._id }, { $set: item });
+};
+
+exports.deleteOneById = async (id) => {
+    const db = await mongoHelper.getDb();
+    return db.collection(collection).deleteOne({ _id: new ObjectID(id) });
 };
