30f4bb024754629e8b5c1a2714fa34b830bf3666
lmaran <lucian.maran@outlook.com>
2021-04-25 14:31:45 +0000

    rename tests -> contests [skip ci]

---
+++ public/js/contest/contest-create-or-edit.event-handler.js
@@ -0,0 +1,25 @@
+/**
+ * event handlers (alias 'controller')
+ */
+export const eventHandlers = {
+    setDefaultContestName: async (event) => {
+        const selectedContestType = event.target.value;
+        const selectedContestName = event.target.options[event.target.selectedIndex].text;
+        const contestNameInput = document.getElementById("contestNameInput");
+
+        switch (selectedContestType) {
+            case "olimpiada-locala":
+                contestNameInput.value = `${selectedContestName}, <judet>, <anul>`;
+                break;
+            case "olimpiada-judeteana":
+            case "olimpiada-nationala":
+                contestNameInput.value = `${selectedContestName}, <anul>`;
+                break;
+            case "alte-concursuri":
+                contestNameInput.value = "Concurs <nume-concurs>, <oras>, <anul>";
+                break;
+            default:
+                contestNameInput.value = "";
+        }
+    },
+};
---
+++ services/contest.service.js
@@ -0,0 +1,31 @@
+const mongoHelper = require("../helpers/mongo.helper");
+const { ObjectID } = require("mongodb");
+
+const collection = "contests";
+
+exports.getAll = async () => {
+    const db = await mongoHelper.getDb();
+    return db.collection(collection).find().toArray();
+};
+
+exports.getOneById = async (id) => {
+    const db = await mongoHelper.getDb();
+    return db.collection(collection).findOne({ _id: new ObjectID(id) });
+};
+
+exports.deleteOneById = async (id) => {
+    const db = await mongoHelper.getDb();
+    return db.collection(collection).deleteOne({ _id: new ObjectID(id) });
+};
+
+exports.insertOne = async (item) => {
+    const db = await mongoHelper.getDb();
+    item.createdOn = new Date();
+    return db.collection(collection).insertOne(item);
+};
+
+exports.updateOne = async (item) => {
+    const db = await mongoHelper.getDb();
+    item._id = new ObjectID(item._id);
+    return db.collection(collection).updateOne({ _id: item._id }, { $set: item });
+};
