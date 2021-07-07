777b7ef2a28259af9c67a1c94d5af049a2f51825
Lucian Maran <lucian.maran@outlook.com>
2021-07-07 15:23:44 +0300

    fix display user as an entity

--- src/middlewares/set-context.middleware.js
+++ src/middlewares/set-context.middleware.js
@@ -1,10 +1,8 @@
 const urlHelper = require("../helpers/url.helper");
 
 exports.setContext = async (req, res, next) => {
-    req.ctx = {};
-
-    req.ctx.user = req.user;
-
+    // req.locals are global variables used by handlebars (called as @root.user, @root.redirectUri etc)
+    res.locals.user = req.user;
     res.locals.redirectUri = urlHelper.getCurrentEncodedUri(req); // e.g: /students?active=true
     res.locals.isNotRootPath = !urlHelper.isRootPath(req);
 
