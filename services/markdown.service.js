8009eadbd6e6056f2127f948c20fa43f75595c6f
lmaran <lucian.maran@outlook.com>
2021-08-08 18:27:43 +0000

    update class description [skip ci]

--- services/markdown.service.js
+++ services/markdown.service.js
@@ -1,10 +1,27 @@
 const katex = require("katex");
 const tm = require("markdown-it-texmath");
-const md = require("markdown-it")().use(tm, {
-    engine: katex,
-    delimiters: "dollars",
-    katexOptions: { macros: { "\\RR": "\\mathbb{R}" }, minRuleThickness: 0.05, fleqn: true },
-});
+
+// markdown-it-container - Plugin for creating block-level custom containers
+// https://github.com/markdown-it/markdown-it-container
+const ct = require("markdown-it-container");
+
+// markdown-it-attrs - Add classes, identifiers and attributes to your markdown with {.class #identifier attr=value attr2="spaced value"} curly brackets
+// https://www.npmjs.com/package/@gerhobbelt/markdown-it-attrs
+const markdownItAttrs = require("@gerhobbelt/markdown-it-attrs");
+const md = require("markdown-it")()
+    .use(tm, {
+        engine: katex,
+        delimiters: "dollars",
+        katexOptions: { macros: { "\\RR": "\\mathbb{R}" }, minRuleThickness: 0.05, fleqn: true },
+    })
+    //.use(ct, "divContainer") // I don't use it for now...maybe useful to render a list (or any block) inside a cell table
+    .use(markdownItAttrs);
+
+// use of "divContainer"
+// ::: divContainer {.text-success}
+// *here be dragons*
+// :::
+
 // added "minRuleThickness" option because dividing lines for \frac and \dfrac sometimes disappear (Chrome, zoom 100%)
 // default value is 0.04 (as defined in Latex specs) and seems that Chrome rounds down  sub-pixel lines
 // https://github.com/KaTeX/KaTeX/issues/1775#issuecomment-449762459
@@ -17,5 +34,9 @@ const md = require("markdown-it")().use(tm, {
 // https://tex.stackexchange.com/questions/28650/how-can-i-use-an-align-environment-flush-left
 
 exports.render = (katex) => {
+    md.renderer.rules.table_open = function () {
+        return '<table class="table">';
+    };
+
     return md.render(katex);
 };
