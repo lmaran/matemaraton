dd5e5aec4a9bee8a4534afb1f3a0f68b110bbff4
Lucian Maran <lucian.maran@outlook.com>
2021-09-12 22:23:14 +0300

    add classes for edition 5

--- src/controllers/enrollment.controller.js
+++ src/controllers/enrollment.controller.js
@@ -27,7 +27,7 @@ exports.enrollInClassGet = async (req, res) => {
         data.className = cls.name;
         data.classLevel = cls.level;
         data.classEnrollStatus = cls.enrollmentInfo && cls.enrollmentInfo.status;
-        data.classIsActive = cls.isActive;
+        data.classIsActive = !cls.isCompleted;
     } else {
         //redirect after validation
         data = initialValues[0];
@@ -58,16 +58,12 @@ exports.enrollInClassPost = async (req, res) => {
 
         // dynamic validations
         if (!req.user.isAdmin) {
-            const enrollRequestsByParent = await enrollService.getAllByClassIdAndParentId(
-                req.body.classId,
-                req.user._id
-            );
+            const enrollRequestsByParent = await enrollService.getAllByClassIdAndParentId(req.body.classId, req.user._id);
             if (enrollRequestsByParent.length > 0) {
                 const validationErrors = [
                     {
                         page: {
-                            msg:
-                                "Puteți înscrie maxim un elev (propriul copil). In cazul în care aveți doi copii pe care doriți să-i înscrieți la aceeași clasă (ex: gemeni), vom trata acest caz ca pe o excepție. In acest sens, vă rugăm să trimiteți datele celui de-al 2-lea copil pe email, la adresa lucian.maran@gmail.com.",
+                            msg: "Puteți înscrie maxim un elev (propriul copil). In cazul în care aveți doi copii pe care doriți să-i înscrieți la aceeași clasă (ex: gemeni), vom trata acest caz ca pe o excepție. In acest sens, vă rugăm să trimiteți datele celui de-al 2-lea copil pe email, la adresa lucian.maran@gmail.com.",
                         },
                     },
                 ];
@@ -122,10 +118,7 @@ exports.enrollInClassPost = async (req, res) => {
 exports.getAllPerClass = async (req, res) => {
     const classId = req.params.classId;
 
-    const [cls, enrollments] = await Promise.all([
-        await classService.getOneById(classId),
-        await enrollService.getAllByClassId(classId),
-    ]);
+    const [cls, enrollments] = await Promise.all([await classService.getOneById(classId), await enrollService.getAllByClassId(classId)]);
     // const enrollments = await enrollService.getAllByClassId(classId);
 
     enrollments.forEach((x) => {
@@ -136,7 +129,7 @@ exports.getAllPerClass = async (req, res) => {
         const createdOnAsLocal = new Date(createdOnAsUtc.getTime() - offset * 60000); // https://docs.mongodb.com/manual/tutorial/model-time-data/#example
 
         x.createdOn = dateTimeHelper.getShortDateAndTimeDate(createdOnAsLocal);
-        
+
         x.studentShortName = `${x.studentFirstName} ${x.studentLastName.charAt(0)}.`;
     });
 
@@ -162,14 +155,7 @@ exports.getAllPerClass = async (req, res) => {
 function convertDateToUTC(date) {
     // TODO: move this part in date-time helper
     // https://stackoverflow.com/a/14006555
-    return new Date(
-        date.getUTCFullYear(),
-        date.getUTCMonth(),
-        date.getUTCDate(),
-        date.getUTCHours(),
-        date.getUTCMinutes(),
-        date.getUTCSeconds()
-    );
+    return new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds());
 }
 
 const getStaticValidationErrors = (req) => {
@@ -179,28 +165,23 @@ const getStaticValidationErrors = (req) => {
 
         // studentLastName
         if (validator.isEmpty(studentLastName)) validationErrors.studentLastName = { msg: "Câmp obligatoriu" };
-        else if (!validator.isLength(studentLastName, { max: 50 }))
-            validationErrors.studentLastName = { msg: "Maxim 50 caractere" };
+        else if (!validator.isLength(studentLastName, { max: 50 })) validationErrors.studentLastName = { msg: "Maxim 50 caractere" };
 
         // studentFirstName
         if (validator.isEmpty(studentFirstName)) validationErrors.studentFirstName = { msg: "Câmp obligatoriu" };
-        else if (!validator.isLength(studentFirstName, { max: 50 }))
-            validationErrors.studentFirstName = { msg: "Maxim 50 caractere" };
+        else if (!validator.isLength(studentFirstName, { max: 50 })) validationErrors.studentFirstName = { msg: "Maxim 50 caractere" };
 
         // mathAvgGrade1
-        if (!validator.isLength(mathAvgGrade1, { max: 50 }))
-            validationErrors.mathAvgGrade1 = { msg: "Maxim 50 caractere" };
+        if (!validator.isLength(mathAvgGrade1, { max: 50 })) validationErrors.mathAvgGrade1 = { msg: "Maxim 50 caractere" };
 
         // mathAvgGrade2
-        if (!validator.isLength(mathAvgGrade2, { max: 50 }))
-            validationErrors.mathAvgGrade2 = { msg: "Maxim 50 caractere" };
+        if (!validator.isLength(mathAvgGrade2, { max: 50 })) validationErrors.mathAvgGrade2 = { msg: "Maxim 50 caractere" };
 
         // schoolName
         if (!validator.isLength(schoolName, { max: 50 })) validationErrors.schoolName = { msg: "Maxim 50 caractere" };
 
         // observations
-        if (!validator.isLength(observations, { max: 250 }))
-            validationErrors.observations = { msg: "Maxim 250 caractere" };
+        if (!validator.isLength(observations, { max: 250 })) validationErrors.observations = { msg: "Maxim 250 caractere" };
 
         return validationErrors;
     } catch (err) {
