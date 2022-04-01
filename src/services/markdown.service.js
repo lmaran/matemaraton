7bed1b18e9610c414c58cc3c08d258fa23c59c27
Lucian Maran <lucian.maran@outlook.com>
2022-04-01 18:25:34 +0300

    update npm packages

--- src/services/markdown.service.js
+++ src/services/markdown.service.js
@@ -1,26 +1,11 @@
 const katex = require("katex");
 const tm = require("markdown-it-texmath");
 
-// markdown-it-container - Plugin for creating block-level custom containers
-// https://github.com/markdown-it/markdown-it-container
-const ct = require("markdown-it-container");
-
-// markdown-it-attrs - Add classes, identifiers and attributes to your markdown with {.class #identifier attr=value attr2="spaced value"} curly brackets
-// https://www.npmjs.com/package/@gerhobbelt/markdown-it-attrs
-const markdownItAttrs = require("@gerhobbelt/markdown-it-attrs");
-const md = require("markdown-it")()
-    .use(tm, {
-        engine: katex,
-        delimiters: "dollars",
-        katexOptions: { macros: { "\\RR": "\\mathbb{R}" }, minRuleThickness: 0.05, fleqn: true },
-    })
-    //.use(ct, "divContainer") // I don't use it for now...maybe useful to render a list (or any block) inside a cell table
-    .use(markdownItAttrs);
-
-// use of "divContainer"
-// ::: divContainer {.text-success}
-// *here be dragons*
-// :::
+const md = require("markdown-it")().use(tm, {
+    engine: katex,
+    delimiters: "dollars",
+    katexOptions: { macros: { "\\RR": "\\mathbb{R}" }, minRuleThickness: 0.05, fleqn: true, throwOnError: false },
+});
 
 // added "minRuleThickness" option because dividing lines for \frac and \dfrac sometimes disappear (Chrome, zoom 100%)
 // default value is 0.04 (as defined in Latex specs) and seems that Chrome rounds down  sub-pixel lines
@@ -33,10 +18,11 @@ const md = require("markdown-it")()
 // in Latex, there is a package (fleqn) for left align:
 // https://tex.stackexchange.com/questions/28650/how-can-i-use-an-align-environment-flush-left
 
-exports.render = (katex) => {
+exports.render = (source) => {
     md.renderer.rules.table_open = function () {
         return '<table class="table">';
     };
 
-    return md.render(katex);
+    //console.log(source);
+    return md.render(source);
 };
