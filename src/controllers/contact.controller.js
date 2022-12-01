e145872cfd86f4b07f2bb59034a14259b4e87e86
Lucian Maran <lucian.maran@outlook.com>
2022-12-01 17:10:08 +0200

    simplify home page

---
+++ src/controllers/contact.controller.js
@@ -0,0 +1,8 @@
+exports.getContactPage = async (req, res) => {
+    // const data = {
+    //     ctx: req.ctx,
+    // };
+    // res.render("contact", data);
+    // console.log(res.locals);
+    res.render("contact");
+};
