1b74a9b41f5e6a225f377fcf43ccc3d52d49d691
lmaran <lucian.maran@outlook.com>
2023-11-16 12:34:31 +0000

    .

--- public/js/eventHandlers/main.event-handler.js
+++ public/js/eventHandlers/main.event-handler.js
@@ -7,6 +7,18 @@ export const eventHandler = {
         window.dispatchEvent(new Event("storage"));
     },
 
+    deleteCartItem: async (event, exerciseId) => {
+        event.preventDefault(); // prevent calling the href
+        event.stopPropagation(); // prevent closing the dropdown menu
+
+        let cartItems = JSON.parse(localStorage.getItem("cart")) || [];
+        cartItems = cartItems.filter((x) => x.exerciseId !== exerciseId);
+        localStorage.setItem("cart", JSON.stringify(cartItems));
+
+        // in main.event-handler.js we have a listener that updates the chart buttons and the chart icon: https://stackoverflow.com/a/65348883
+        window.dispatchEvent(new Event("storage"));
+    },
+
     updateCartIcon: async () => {
         const cartItems = JSON.parse(localStorage.getItem("cart")) || [];
         if (cartItems) {
@@ -44,10 +56,16 @@ export const eventHandler = {
             cartItems.forEach((x) => {
                 const li = document.createElement("li");
                 const a = document.createElement("a");
-                a.classList.add("dropdown-item", "dynamic-node");
+                a.classList.add("dropdown-item", "dynamic-node", "pe-2");
                 a.setAttribute("href", `/cursuri/${x.courseId}/exercitii/${x.exerciseId}`);
                 a.innerText = `E.${x.exerciseCode}`;
-                // a.title = x.exerciseId;
+
+                const i = document.createElement("i");
+                i.classList.add("bi", "bi-x", "float-end");
+                // How to pass arguments to addEventListener: https://stackoverflow.com/a/55859799
+                i.addEventListener("click", (evt) => eventHandler.deleteCartItem(evt, x.exerciseId));
+
+                a.appendChild(i);
 
                 li.appendChild(a);
 
