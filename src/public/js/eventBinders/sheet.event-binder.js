0000b599aafd17232c04d7577613b7534c34669a
Lucian Maran <lucian.maran@outlook.com>
2023-11-20 11:30:49 +0200

    Create a general sheet from cart

---
+++ src/public/js/eventBinders/sheet.event-binder.js
@@ -0,0 +1,14 @@
+import { commonEventHandler } from "../eventHandlers/course-lesson.common-event-handler.js";
+// import { eventHandlers } from "../eventHandlers/course-lesson-create-or-edit.event-handler.js";
+// import { dateTimeHelper } from "../helpers/date-time.helper.js";
+
+// document.getElementById("toggle-theory-editor-btn").addEventListener("click", eventHandlers.toggleTheoryEditor);
+// document.getElementById("theory-editor-txt").addEventListener("keyup", dateTimeHelper.debounce(eventHandlers.getTheoryPreview, 500)); // with debouncer (500 ms)
+
+// common
+// document.querySelectorAll(".toggle-section-btn").forEach((item) => item.addEventListener("click", commonEventHandler.toggleSection));
+
+document.querySelectorAll(".toggle-answer-btn").forEach((item) => item.addEventListener("click", commonEventHandler.toggleAnswer));
+document.querySelectorAll(".toggle-hints-btn").forEach((item) => item.addEventListener("click", commonEventHandler.toggleHints));
+document.querySelectorAll(".show-next-hint-btn").forEach((item) => item.addEventListener("click", commonEventHandler.showNextHint));
+document.querySelectorAll(".toggle-solution-btn").forEach((item) => item.addEventListener("click", commonEventHandler.toggleSolution));
