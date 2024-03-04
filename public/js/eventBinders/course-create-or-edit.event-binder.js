d407a10ee55935bba743285b02c2364d8fdbd1a8
lmaran <lucian.maran@outlook.com>
2024-03-04 15:52:02 +0000

    .

--- public/js/eventBinders/course-create-or-edit.event-binder.js
+++ public/js/eventBinders/course-create-or-edit.event-binder.js
@@ -1,3 +1,8 @@
 import { eventHandlers } from "../eventHandlers/course-create-or-edit.event-handler.js";
+import { dateTimeHelper } from "../helpers/date-time.helper.js";
 
 document.getElementById("sectionSelect").addEventListener("change", eventHandlers.setDefaultSection);
+
+document.getElementById("toggle-description-editor-btn")?.addEventListener("click", eventHandlers.toggleDescriptionEditor);
+const descriptionEditorTxt = document.getElementById("description-editor-txt");
+descriptionEditorTxt?.addEventListener("keyup", dateTimeHelper.debounce(eventHandlers.getDescriptionPreview, 500)); // with debouncer (500 ms)
--- public/js/eventHandlers/course-create-or-edit.event-handler.js
+++ public/js/eventHandlers/course-create-or-edit.event-handler.js
@@ -1,9 +1,26 @@
 import { sectionService } from "../services/section.service.js";
+import { markdownService } from "../services/markdown.service.js";
+
+const editorTxt = document.getElementById("description-editor-txt");
+const descriptionPreviewDiv = document.getElementById("description-preview-div");
 
 export const eventHandlers = {
     setDefaultSection: async (event) => {
         event.preventDefault();
         await setDefaultPosition();
+    },
+    toggleDescriptionEditor: async (event) => {
+        event.preventDefault();
+
+        editorTxt.classList.toggle("d-none");
+
+        const editorIsHide = editorTxt.classList.contains("d-none");
+        event.target.textContent = editorIsHide ? "Editează" : "Ascunde";
+        descriptionPreviewDiv.style.borderTopStyle = editorIsHide ? "solid" : "dashed";
+    },
+    getDescriptionPreview: async (event) => {
+        const data = { markdown: event.target.value };
+        descriptionPreviewDiv.innerHTML = await markdownService.getRenderedMarkdown(data);
     },
 };
 
